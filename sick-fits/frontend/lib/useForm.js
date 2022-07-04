8ba7e5c0de90809deb5158608153728f6cd7096e
João Barradas <joao.barradas@jstack.eu>
2022-07-04 21:59:17 +0100

    adv react 23

--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function useForm(initial = {}) {
   // create a state obj for our inputs
   const [inputs, setInputs] = useState(initial);
+  const initialValues = Object.values(initial).join('');
+
+  useEffect(() => {
+    // This function runs when the things we are watching change
+    setInputs(initial);
+  }, [initialValues]);
 
   function handleChange(e) {
     let { value, name, type } = e.target;
 
     if (type === 'number') value = parseInt(value);
-    if (type === 'file') value = e.target.files;
+    if (type === 'file') [value] = e.target.files
 
     setInputs({
       ...inputs,
